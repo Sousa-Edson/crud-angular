4c30599065eeec7f99c18baaef1679fb07c4e1a0
Edson Sousa <edson3711.es@gmail.com>
2024-01-29 16:24:17 -0300

    validando tamanho do campo nome

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -25,7 +25,7 @@ export class CourseFormComponent implements OnInit {
     _id: [''],
     name: [
       '',
-      [Validators.required, Validators.min(5), Validators.maxLength(100)],
+      [Validators.required, Validators.minLength(5), Validators.maxLength(100)],
     ],
     category: ['', [Validators.required]],
   });
@@ -74,8 +74,17 @@ export class CourseFormComponent implements OnInit {
   getErrorMessage(fieldName: string) {
     const field = this.form.get(fieldName);
     if (field?.hasError('required')) {
-      return 'Campo Obigatório';
+      return 'Campo Obrigatório';
     }
-    return 'Erro';
+    if (field?.hasError('minlength')) {
+      const requiredLength:number = field.errors ? field.errors['minlength']['requiredLength']:5
+      return `Tamanho minimo precisa ser de ${requiredLength} caracteres.`;
+    }
+    if (field?.hasError('maxlength')) {
+      const requiredLength:number = field.errors ? field.errors['maxlength']['requiredLength']:100
+      return `Tamanho máximo excedido de ${requiredLength} caracteres.`;
+    }
+
+    return 'Campo inválido';
   }
 }
