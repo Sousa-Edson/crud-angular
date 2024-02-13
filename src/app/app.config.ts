d8c79779a5f20b6bf4f1b4805f90be13a913a91f
Edson Sousa <edson3711.es@gmail.com>
2024-02-13 18:53:05 -0300

    migração para standalone

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,13 @@
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient, withFetch, withInterceptorsFromDi } from '@angular/common/http';
+import { PreloadAllModules, provideRouter, withPreloading } from '@angular/router';
+
+import { APP_ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimations(),provideHttpClient(withFetch() )
+  providers: [provideRouter(APP_ROUTES,withPreloading(PreloadAllModules)), provideClientHydration(), provideAnimations(),provideHttpClient(withFetch() )
   ]
 };
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
+import { CoursesComponent } from './courses/containers/courses/courses.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
+export const APP_ROUTES: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'courses' }
   ,
   {
     path: 'courses',
-    loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule)
+    loadChildren: () => import('./courses/courses.routes').then(m => m.COUSES_ROUTES)
+  },{
+    path: 'student',
+    loadComponent: () => import('./courses/containers/course-form/course-form.component').then(m => m.CourseFormComponent)
   }
 ];
